e0c2a79a73a29f6af9d67357a13a06d486864393
gabrieldpbarros <121111464+gabrieldpbarros@users.noreply.github.com>
2024-11-09 22:35:08 -0300

    Update 2729.c
    O beecrowd parece ainda identificar algum erro, mas não faço ideia do que deve ser.

--- beecrowd/2729.c
+++ beecrowd/2729.c
@@ -9,8 +9,10 @@ typedef struct listaCompras{
 
 void imprimeLista(celula *lst){
     celula *aux;
-    for(aux = lst->seg; aux != NULL; aux = aux->seg)
-        printf("%s ", aux->compras);
+    for(aux = lst->seg; aux != NULL; aux = aux->seg){
+        printf("%s", aux->compras);
+        if(aux->seg != NULL) printf(" ");
+    }
     printf("\n");
 }
 
@@ -20,25 +22,47 @@ int repeticao(celula *lst, char *item){
         if(strcmp(aux->compras, item) == 0) return 1;   
     return 0;
 }
+// Algoritmo de bubble sort.
+void ordenaLista(celula *lst){ // O algoritmo considera que a lista existe.
+    celula *i, *j;
+    char temp[20];
+
+    for(i = lst->seg; i != NULL; i = i->seg){
+        for(j = i->seg; j != NULL; j = j->seg){
+            if(strcmp(i->compras, j->compras) > 0){
+                strcpy(temp, i->compras);
+                strcpy(i->compras, j->compras);
+                strcpy(j->compras, temp);
+            }
+        }
+    }
+}
 
 int main(void){
     // Declaração das variáveis.
     int qtdCompras, i;
-    char *itens, *item;
+    char *itens, item[20];
     celula *lst, *lstnew, *aux;
-    lst = malloc(sizeof(celula));
-    lst->seg = NULL;
+    celula **listas; // Armazena as listas independentes.
+
     // Recebe a quantidade de listas.
     scanf("%d", &qtdCompras);
     getchar(); // Descarta o caractere de nova linha.
 
     itens = malloc(1000);
-    item = malloc(20);
+    listas = malloc(qtdCompras * sizeof(celula*));
 
     for(i = 0; i < qtdCompras; i++){
+        // Variáveis auxiliares.
+        int j = 0, k = 0;
+
+        // Cria a lista cabeça.
+        lst = malloc(sizeof(celula));
+        lst->seg = NULL;
+
+        // Recebe a lista do usuário.
         fgets(itens, 1000, stdin);
 
-        int j = 0, k = 0;
 
         while(itens[j] != '\0'){
             if(itens[j] == ' ' || itens[j] == '\n'){
@@ -47,11 +71,12 @@ int main(void){
                 if(!repeticao(lst, item)){
                     // Cria uma lista contendo apenas o último produto lido.
                     lstnew = malloc(sizeof(celula));
+
                     // Insere o produto na lista.
-                    for(int l = 0; l < k; l++)
+                    for(int l = 0; item[l] != '\0'; l++)
                         lstnew->compras[l] = item[l];
-                    lstnew->compras[k] = ' ';
-                    lst->seg = lstnew;
+                    lstnew->seg = NULL;
+                    lstnew->compras[k] = '\0';
 
                     // Percorre toda a lista e adiciona o produto ao final da lista.
                     aux = lst;
@@ -63,26 +88,29 @@ int main(void){
 
                 k = 0;
             } else{
-                item[k] = itens[j];
-                k++;
+                item[k++] = itens[j];
             }
-
             j++;
         }
+        ordenaLista(lst);
+
+        listas[i] = lst;
     }
     
-    imprimeLista(lst);
+    for(i = 0; i < qtdCompras; i++) imprimeLista(listas[i]);
     
     // Liberação dos espaços de memória alocados.
-    free(itens);
-    free(item);
-
-    aux = lst;
-    while(aux != NULL){
-        celula *temp = aux;
-        aux = aux->seg;
-        free(temp);
+    for(i = 0; i < qtdCompras; i++){
+        aux = listas[i];
+        while(aux != NULL){
+            celula *temp = aux;
+            aux = aux->seg;
+            free(temp);
+        }
+        
+        free(listas[i]);
     }
+    free(itens);
 
     return 0;
 }
