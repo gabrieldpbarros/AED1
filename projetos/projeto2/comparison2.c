170b7e36eaaa338c6dd2985a0aee6fb5d65c624d
Gabriel D. <gabrielpanovich@gmail.com>
2024-12-08 18:14:20 -0300

    modified:   comparison2.c 	modified:   comparison2.exe

--- projetos/projeto2/comparison2.c
+++ projetos/projeto2/comparison2.c
@@ -3,7 +3,7 @@
 #include "mergesort.h"
 #include <time.h> // clock(), CLOCKS_PER_SEC e clock_t
 
-#define TAM 180000 // Constante para tamanho do vetor
+#define TAM 20000 // Constante para tamanho do vetor
 
 void initializeRandom(void){
     srand((unsigned) time(NULL));
@@ -11,6 +11,29 @@ void initializeRandom(void){
 
 int randomInteger(int low, int high){
     return rand() % (high - low + 1) + low;
+}
+
+void printAll(int *v1, int *v2, int *v3, int size, clock_t start, clock_t end){
+    // Verificando tempo de execução do algoritmo de ordenação => t1
+    start = clock(); // Armazena tempo
+    insertion(v1, TAM);
+    end = clock(); // tempo final - tempo inicial
+    // Imprime o tempo na tela
+    printf("Tempo de execucao (Insertion Sort): %lf\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
+
+    // Verificando tempo de execução do algoritmo de ordenação => t2
+    start = clock(); // Armazena tempo
+    merge(v2, 0, size);
+    end = clock(); // tempo final - tempo inicial
+    // Imprime o tempo na tela
+    printf("Tempo de execucao (Merge Sort): %lf\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
+
+    // Verificando tempo de execução do algoritmo de ordenação => t3w
+    start = clock(); // Armazena tempo
+    merge(v3, 0, size);
+    end = clock(); // tempo final - tempo inicial
+    // Imprime o tempo na tela
+    printf("Tempo de execucao (Quick Sort): %lf\n\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
 }
 
 int main(void){
@@ -24,30 +47,12 @@ int main(void){
     initializeRandom();
 
     // Geração aleatória dos valores do vetor
-    for(a = 0; a < TAM; a++){
-        vetor1[a] = randomInteger(0, TAM);
-        vetor2[a] = vetor1[a];
-        vetor3[a] = vetor1[a];
+    for(int i = 0; i < 5; i++){
+        for(a = 0; a < TAM; a++){
+            vetor1[a] = randomInteger(0, TAM);
+            vetor2[a] = vetor1[a];
+            vetor3[a] = vetor1[a];
+        }
+        printAll(vetor1, vetor2, vetor3, r, start, end);
     }
-
-    // Verificando tempo de execução do algoritmo de ordenação => t1
-    start = clock(); // Armazena tempo
-    insertion(vetor1, TAM);
-    end = clock(); // tempo final - tempo inicial
-    // Imprime o tempo na tela
-    printf("Tempo de execucao (Insertion Sort): %lf\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
-
-    // Verificando tempo de execução do algoritmo de ordenação => t2
-    start = clock(); // Armazena tempo
-    merge(vetor2, 0, r);
-    end = clock(); // tempo final - tempo inicial
-    // Imprime o tempo na tela
-    printf("Tempo de execucao (Merge Sort): %lf\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
-
-    // Verificando tempo de execução do algoritmo de ordenação => t3w
-    start = clock(); // Armazena tempo
-    merge(vetor3, 0, r);
-    end = clock(); // tempo final - tempo inicial
-    // Imprime o tempo na tela
-    printf("Tempo de execucao (Quick Sort): %lf\n", (double) (end - start) / CLOCKS_PER_SEC); //Conversão para segundos
 }
